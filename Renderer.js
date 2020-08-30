23f05fd22af0a794210277a546243853a21e0335
roiby <roiby92@gmail.com>
2020-08-30 18:37:58 +0300

    pushed after fixsing bugs

--- Renderer.js
+++ Renderer.js
@@ -1,13 +1,6 @@
 class Renderer {
-    
-    // render(user,friends,kanyeQoute,bacon,favoritPokemon){
-    //     this.renderUser(user),
-    //     this.randerFriends(friends)
-    //     this.renderQoute(kanyeQoute)
-    //     this.renderBacon(bacon)
-    //     this.renderPokemon(favoritPokemon)
-    // }
-    renderUser(user){
+
+    renderUser(user) {
         $(".user-container").empty()
         const source = $('#user-template').html()
         const template = Handlebars.compile(source);
@@ -15,36 +8,58 @@ class Renderer {
         $('.user-container').append(userHtml)
     }
 
-    renderFriends(friends){
+    renderFriends(friends) {
         $(".friends-container").empty()
         const source = $('#friends-template').html();
         const template = Handlebars.compile(source);
-        const friendsHtml = template({friends});
+        const friendsHtml = template({ friends });
         $('.friends-container').append(friendsHtml)
     }
 
-    renderQuote(kanyeQuote){
+    renderQuote(kanyeQuote) {
         $(".quote-container").empty()
         const source = $('#quote-template').html();
         const template = Handlebars.compile(source);
         const qouteHtml = template(kanyeQuote);
-        $('.quote-container').append(qouteHtml) 
+        $('.quote-container').append(qouteHtml)
     }
-
-    renderBacon(bacon){
+    renderBacon(bacon) {
         $(".meat-container").empty()
         const source = $('#bacon-template').html();
         const template = Handlebars.compile(source);
         const meatHtml = template(bacon);
-        $('.meat-container').append(meatHtml)  
+        $('.meat-container').append(meatHtml)
     }
 
-    renderPokemon(favoritPokemon){
+    renderPokemon(favoritPokemon) {
         $(".pokemon-container").empty()
         const source = $('#pokemon-template').html();
         const template = Handlebars.compile(source);
         const pokemonHtml = template(favoritPokemon);
-        $('.pokemon-container').append(pokemonHtml) 
+        $('.pokemon-container').append(pokemonHtml)
     }
 
 }
+
+// render(containerId, tEmplate, data,) {
+//     $(`${containerId}`).empty()
+//     const source = $(`${tEmplate}`).html()
+//     const template = Handlebars.compile(source);
+//     const newHtml = template(data);
+//     $(`${containerId}`).append(newHtml)
+// }
+// renderFriends(Data) {
+//     this.render(".friends-container", '#friends-template',  {Data})
+// }
+// renderUser(Data) {
+//     this.render(".user-container", '#user-template', Data)
+// }
+// renderQuote(Data) {
+//     this.render(".quote-container", '#quote-template', Data)
+// }
+// renderBacon(Data){
+//     this.render(".meat-container", '#bacon-template', Data)
+// }
+// renderPokemon(Data){
+//     this.render(".pokemon-container", '#pokemon-template',Data)
+// }
--- main.js
+++ main.js
@@ -8,8 +8,13 @@ $('#load').on('click',function(){
 
 $('#display').on('click' ,function(){
     renderer.renderUser(user.userData.user)
-    renderer.renderQuote(user.userData.kanyeQuote.quote)
     renderer.renderFriends(user.userData.friends)
+    renderer.renderQuote(user.userData.kanyeQuote.quote)
     renderer.renderBacon(user.userData.bacon.meat)
     renderer.renderPokemon(user.userData.favoritPokemon)
 })
+// renderer.renderUser(".user-container", '#user-template', user.userData.user)
+// renderer.renderFriends(".friends-container", '#friends-template',  user.userData.friends)
+// renderer.renderQuote(".quote-container", '#quote-template', user.userData.kanyeQuote)
+// renderer.renderBacon(".meat-container", '#bacon-template', user.userData.bacon)
+// renderer.renderPokemon(".pokemon-container", '#pokemon-template', user.userData.favoritPokemon)
